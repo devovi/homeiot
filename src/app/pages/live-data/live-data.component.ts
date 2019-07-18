c5e7959f057f4c9a3649a4749643c18132f5bc89
Sivani Jaladi <sivanijaladi@Sivanis-MacBook-Pro.local>
2019-07-18 20:05:03 +0530

    live data graphs updated

--- src/app/pages/live-data/live-data.component.ts
+++ src/app/pages/live-data/live-data.component.ts
@@ -1,15 +1,153 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Chart} from 'chart.js';
 @Component({
   selector: 'app-live-data',
   templateUrl: './live-data.component.html',
   styleUrls: ['./live-data.component.scss']
 })
 export class LiveDataComponent implements OnInit {
-
+ LineChart;
+ BarChart;
   constructor() { }
 
   ngOnInit() {
+    this.LineChart = new Chart(`lineChart` , {
+    type: `line`,
+    
+    data: {
+      labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+      datasets:[{
+        label: "Temperature (°C)",
+        data: [9,2,4,6,5,7,8,9,3],
+        fill:false,
+        lineTension:0.2,
+        borderColor: 'red',
+        borderWidth:1
+      }]
+    },
+    options:{
+       title:{
+          // text: "Line Chart",
+          display: true
+       },
+       scales:{
+         yAxes: [{
+             ticks:{
+               
+               beginAtZero:true
+             }
+         }]
+       }
+    } 
+    });
+    this.LineChart = new Chart(`lineChart1` , {
+      type: `line`,
+      
+      data: {
+        labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+        datasets:[{
+          // label: "Temperature (°C)",
+          data: [9,2,4,6,5,7,8,9,3],
+          fill:false,
+          lineTension:0.2,
+          borderColor: 'red',
+          borderWidth:1
+        }]
+      },
+      options:{
+         title:{
+            // text: "Line Chart",
+            display: true
+         },
+         scales:{
+           yAxes: [{
+               ticks:{
+                 
+                 beginAtZero:true
+               }
+           }]
+         }
+      } 
+      });
+
+    this.BarChart = new Chart ( 'barChart', {
+      type: 'bar' , 
+      data:{
+        labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+        datasets:[{
+          label:"count",
+          data : [1,2,3,4,5,6,7],
+          backgroundColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)",
+
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)"
+          ],
+          borderColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",,
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",
+          ],
+        }]
+        },
+        options:{
+          title:{
+            //  text: "bar Chart",
+             display: true
+          },
+          scales:{
+            yAxes: [{
+                ticks:{
+                  
+                  beginAtZero:true
+                }
+            }]
+          }
+       } 
+      
+
+    });
+    this.BarChart = new Chart ( 'barChart1', {
+      type: 'bar' , 
+      data:{
+        labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+        datasets:[{
+          label:"count",
+          data : [1,2,3,4,5,6,7],
+          backgroundColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)",
+
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)"
+          ],
+          borderColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",,
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",
+          ],
+        }]
+        },
+        options:{
+          title:{
+             text: "",
+             display: true
+          },
+          scales:{
+            yAxes: [{
+                ticks:{
+                  
+                  beginAtZero:true
+                }
+            }]
+          }
+       } 
+      
+
+    });
   }
 
 }
