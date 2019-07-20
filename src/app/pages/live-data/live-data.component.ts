dc90a5a7c0dd7ed864037a49ac2fd250b574bdec
sivani <shivani@oviya.co>
2019-07-20 14:39:21 +0530

    hall kichen room updated

--- src/app/pages/live-data/live-data.component.ts
+++ src/app/pages/live-data/live-data.component.ts
@@ -148,6 +148,280 @@ export class LiveDataComponent implements OnInit {
       
 
     });
+    this.BarChart = new Chart ( 'kitchen-barChart', {
+      type: 'bar' , 
+      data:{
+        labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+        datasets:[{
+          label:"count",
+          data : [1,2,3,4,5,6,7],
+          backgroundColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)",
+
+            "rgba(255,99,132,0.2)",
+            "rgba(54 ,162 , 235,0.2)"
+          ],
+          borderColor:[
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",,
+            "rgba(255,99,132,0.2)",
+            "rgba(255,99,132,0.2)",
+          ],
+        }]
+        },
+        options:{
+          title:{
+             text: "",
+             display: true
+          },
+          scales:{
+            yAxes: [{
+                ticks:{
+                  
+                  beginAtZero:true
+                }
+            }]
+          }
+       } 
+      
+
+    });
+    this.LineChart = new Chart(`kitchen-lineChart` , {
+      type: `line`,
+      
+      data: {
+        labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+        datasets:[{
+          // label: "Temperature (°C)",
+          data: [9,2,4,6,5,7,8,9,3],
+          fill:false,
+          lineTension:0.2,
+          borderColor: 'red',
+          borderWidth:1
+        }]
+      },
+      options:{
+         title:{
+            // text: "Line Chart",
+            display: true
+         },
+         scales:{
+           yAxes: [{
+               ticks:{
+                 
+                 beginAtZero:true
+               }
+           }]
+         }
+      } 
+      });
+      this.BarChart = new Chart ( 'kitchen-barChart1', {
+        type: 'bar' , 
+        data:{
+          labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+          datasets:[{
+            label:"count",
+            data : [1,2,3,4,5,6,7],
+            backgroundColor:[
+              "rgba(255,99,132,0.2)",
+              "rgba(54 ,162 , 235,0.2)",
+  
+              "rgba(255,99,132,0.2)",
+              "rgba(54 ,162 , 235,0.2)"
+            ],
+            borderColor:[
+              "rgba(255,99,132,0.2)",
+              "rgba(255,99,132,0.2)",,
+              "rgba(255,99,132,0.2)",
+              "rgba(255,99,132,0.2)",
+            ],
+          }]
+          },
+          options:{
+            title:{
+               text: "",
+               display: true
+            },
+            scales:{
+              yAxes: [{
+                  ticks:{
+                    
+                    beginAtZero:true
+                  }
+              }]
+            }
+         } 
+        
+  
+      });
+      this.LineChart = new Chart(`kitchen-lineChart1` , {
+        type: `line`,
+        
+        data: {
+          labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+          datasets:[{
+            // label: "Temperature (°C)",
+            data: [9,2,4,6,5,7,8,9,3],
+            fill:false,
+            lineTension:0.2,
+            borderColor: 'red',
+            borderWidth:1
+          }]
+        },
+        options:{
+           title:{
+              // text: "Line Chart",
+              display: true
+           },
+           scales:{
+             yAxes: [{
+                 ticks:{
+                   
+                   beginAtZero:true
+                 }
+             }]
+           }
+        } 
+        });
+        this.BarChart = new Chart ( 'room-barChart', {
+          type: 'bar' , 
+          data:{
+            labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+            datasets:[{
+              label:"count",
+              data : [1,2,3,4,5,6,7],
+              backgroundColor:[
+                "rgba(255,99,132,0.2)",
+                "rgba(54 ,162 , 235,0.2)",
+    
+                "rgba(255,99,132,0.2)",
+                "rgba(54 ,162 , 235,0.2)"
+              ],
+              borderColor:[
+                "rgba(255,99,132,0.2)",
+                "rgba(255,99,132,0.2)",,
+                "rgba(255,99,132,0.2)",
+                "rgba(255,99,132,0.2)",
+              ],
+            }]
+            },
+            options:{
+              title:{
+                 text: "",
+                 display: true
+              },
+              scales:{
+                yAxes: [{
+                    ticks:{
+                      
+                      beginAtZero:true
+                    }
+                }]
+              }
+           } 
+          
+    
+        });
+        this.LineChart = new Chart(`room-lineChart` , {
+          type: `line`,
+          
+          data: {
+            labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+            datasets:[{
+              // label: "Temperature (°C)",
+              data: [9,2,4,6,5,7,8,9,3],
+              fill:false,
+              lineTension:0.2,
+              borderColor: 'red',
+              borderWidth:1
+            }]
+          },
+          options:{
+             title:{
+                // text: "Line Chart",
+                display: true
+             },
+             scales:{
+               yAxes: [{
+                   ticks:{
+                     
+                     beginAtZero:true
+                   }
+               }]
+             }
+          } 
+          });
+          this.BarChart = new Chart ( 'room-barChart1', {
+            type: 'bar' , 
+            data:{
+              labels: ["1" , "2" ,"3","4","5","6","7","8","9","10"],
+              datasets:[{
+                label:"count",
+                data : [1,2,3,4,5,6,7],
+                backgroundColor:[
+                  "rgba(255,99,132,0.2)",
+                  "rgba(54 ,162 , 235,0.2)",
+      
+                  "rgba(255,99,132,0.2)",
+                  "rgba(54 ,162 , 235,0.2)"
+                ],
+                borderColor:[
+                  "rgba(255,99,132,0.2)",
+                  "rgba(255,99,132,0.2)",,
+                  "rgba(255,99,132,0.2)",
+                  "rgba(255,99,132,0.2)",
+                ],
+              }]
+              },
+              options:{
+                title:{
+                   text: "",
+                   display: true
+                },
+                scales:{
+                  yAxes: [{
+                      ticks:{
+                        
+                        beginAtZero:true
+                      }
+                  }]
+                }
+             } 
+            
+      
+          });
+          this.LineChart = new Chart(`room-lineChart1` , {
+            type: `line`,
+            
+            data: {
+              labels:['1' , '2', '3', '4', '5', '6', '7', '8', '9', '10', '11' , '12'],
+              datasets:[{
+                // label: "Temperature (°C)",
+                data: [9,2,4,6,5,7,8,9,3],
+                fill:false,
+                lineTension:0.2,
+                borderColor: 'red',
+                borderWidth:1
+              }]
+            },
+            options:{
+               title:{
+                  // text: "Line Chart",
+                  display: true
+               },
+               scales:{
+                 yAxes: [{
+                     ticks:{
+                       
+                       beginAtZero:true
+                     }
+                 }]
+               }
+            } 
+            });
+
+
   }
 
 }
